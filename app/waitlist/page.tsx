9036fe54fc1a766c6499b178ff77e6366543ffe1
0jas0jas <ojsa1807@gmail.com>
2025-05-22 11:16:31 -0400

    Fixed waitlist

--- app/waitlist/page.tsx
+++ app/waitlist/page.tsx
@@ -9,12 +9,14 @@ import { SubmitButton } from "@/components/submit-button";
 export default async function Waitlist() {
   return (
     <>
-      <main className="flex-1 flex flex-col gap-6 px-4">
+      <main className="flex-1 rounded-3xl flex flex-col gap-6 p-20  min-h-screen items-center justify-center">
+        <div className="p-20 rounded-3xl bg-black/30 bg-[radial-gradient(ellipse_at_top_left,_var(--tw-gradient-stops))] from-green-600 via-purple-600 to-orange-600">
         <form action={addToWaitlistAction} className="flex flex-col gap-4 mt-6">
-          <label htmlFor="email" className="font-medium">
+          <label htmlFor="email" className="font-medium text-3xl">
             Join the waitlist!
           </label>
-          <Input
+          <Input 
+            className="text-xl"
             type="email"
             name="email"
             placeholder="Enter your email"
@@ -22,6 +24,7 @@ export default async function Waitlist() {
           />
           <SubmitButton>Submit</SubmitButton>
         </form>
+        </div>
       </main>
     </>
   );
