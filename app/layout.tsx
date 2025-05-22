f261287b7d265f5ed557dcd77857973d4c7eddd9
0jas0jas <ojsa1807@gmail.com>
2025-05-22 11:31:19 -0400

    Removed startup garbage for real this time

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,14 +8,15 @@ import { ThemeProvider } from "next-themes";
 import Link from "next/link";
 import "./globals.css";
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
 
 export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  metadataBase: "https://nuclearapp.ca",
+  title: "Nuclear Notes",
+  description: "We are better than paper.",
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
+  },
 };
 
 const geistSans = Geist({
