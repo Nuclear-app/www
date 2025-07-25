19202d6f7cc61e307b84c9f4706c725aae6e2511
Ojas S <ojas1807@gmail.com>
2025-07-25 17:34:45 -0400

    Connect incomplete

--- components/connect.tsx
+++ components/connect.tsx
@@ -1,10 +1,14 @@
 const Connect = () => {
   return (
-    <div className="border-2 h-80 w-full grid grid-cols-3 gap-4">
-      <div className="text-sm text-gray-500 col-span-3 border-2">Connect</div>
-      <div className="col-span-1 border-2 h-full">hi</div>
-      <div className="col-span-1 border-2 h-full">hi</div>
-      <div className="col-span-1 border-2 h-full">hi</div>
+    <div className="border-2 w-full grid grid-cols-3 gap-14 px-28">
+      <div className="text-8xl font-black col-span-3 border-2 p-10">Connect with us</div>
+      <div className="col-span-3 flex flex-row justify-between gap-10">
+        <div className="w-1/5 p-10 rounded-3xl bg-[#9000FF6D]">
+          <div className="text-2xl font-bold">Manual</div>
+        </div>
+        <div className="w-2/5 p-10 rounded-3xl aspect-[2/1] bg-[#00D3BE6D]">hi</div>
+        <div className="w-2/5 p-10 rounded-3xl aspect-[2/1] bg-[#E463006D]">hi</div>
+      </div>
     </div>
   );
 };
--- components/opinions.tsx
+++ components/opinions.tsx
@@ -1,7 +1,7 @@
 const Opinions = () => {
   return (
     <div className="py-28 w-full flex flex-col gap-4 items-start justify-center px-28">
-        <div className="text-8xl font-bold text-center leading-tight">
+        <div className="text-6xl font-bold text-center leading-tight">
           Have opinions? We're all ears.
         </div> 
         <button className="bg-black text-white px-8 py-5 rounded-3xl text-4xl">
