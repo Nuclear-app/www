b845bfab39910f81f9d000d8a4fbaaed352b6dfb
Ojas S <ojas1807@gmail.com>
2025-07-25 17:09:39 -0400

    Opinions

--- components/featureRec.tsx
+++ components/featureRec.tsx
@@ -3,11 +3,12 @@ const FeatureRec = () => {
   return <div className="border-2 h-[900px] w-full grid grid-rows-6 gap-4">
     <div className="flex flex-col gap-2 row-span-3">
       <div className="flex flex-row gap-2">
-        <div className="text-sm text-gray-500">feature</div>
+      <img src="/block.png" alt="Block" className="" />
       </div>
     </div>
     <div className="grid grid-cols-3 gap-2 row-span-3 border-2">
-      <div className="text-2xl font-bold col-span-3 border-2">Feature</div>
+      <div className="text-2xl font-bold col-span-3 border-2">
+      </div>
       <div className="col-span-1 border-2">Feature</div>
       <div className="col-span-1 border-2">Feature</div>
       <div className="col-span-1 border-2">Feature</div>
--- components/opinions.tsx
+++ components/opinions.tsx
@@ -1,7 +1,12 @@
 const Opinions = () => {
   return (
-    <div className="border-2 h-80 w-full flex flex-row gap-4">
-        opinions
+    <div className="py-28 w-full flex flex-col gap-4 items-start justify-center px-28">
+        <div className="text-8xl font-bold text-center leading-tight">
+          Have opinions? We're all ears.
+        </div> 
+        <button className="bg-black text-white px-8 py-5 rounded-3xl text-4xl">
+          Reach Out -{">"} 
+        </button>
     </div>
   );
 };
