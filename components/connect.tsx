58e7bdb556c0c5ce4b96adc0f76c955e92f6fb1e
Ojas S <ojas1807@gmail.com>
2025-07-25 17:35:28 -0400

    Connect incomplete

--- components/connect.tsx
+++ components/connect.tsx
@@ -1,7 +1,7 @@
 const Connect = () => {
   return (
-    <div className="border-2 w-full grid grid-cols-3 gap-14 px-28">
-      <div className="text-8xl font-black col-span-3 border-2 p-10">Connect with us</div>
+    <div className="w-full grid grid-cols-3 gap-14 px-28">
+      <div className="text-8xl font-black col-span-3 p-10">Connect with us</div>
       <div className="col-span-3 flex flex-row justify-between gap-10">
         <div className="w-1/5 p-10 rounded-3xl bg-[#9000FF6D]">
           <div className="text-2xl font-bold">Manual</div>
