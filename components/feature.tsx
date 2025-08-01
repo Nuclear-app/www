ec3f9c900bc9b25381441bb68799a81a8995d170
avan <100318118+avansear@users.noreply.github.com>
2025-08-01 18:37:13 -0500

    les try one ithout lazy loading

--- components/feature.tsx
+++ components/feature.tsx
@@ -18,10 +18,10 @@ const Feature = ({ title, tag_img, tag, description, image, gradientFrom = "#8B5
   
   return (
     <div className="w-full px-4 sm:px-8 md:px-15 lg:px-30 xl:px-90">
-      <img src={image} alt="Block" className="w-full mx-auto h-auto" loading="lazy" />
+      <img src={image} alt="Block" className="w-full mx-auto h-auto" />
       <div className="flex flex-col mt-5">
         <div className="flex flex-row gap-2 items-center justify-center w-fit h-fit">
-          <img src={tag_img} alt="Tag" className="w-4 h-4 lg:w-5 lg:h-5" loading="lazy" />
+          <img src={tag_img} alt="Tag" className="w-4 h-4 lg:w-5 lg:h-5" />
           <div className="text-xs lg:text-sm flex items-center justify-center font-bold" style={gradientStyle}>{tag}</div>
         </div>
         <div className="text-xl sm:text-3xl font-bold">
